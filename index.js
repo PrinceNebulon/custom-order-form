9478ea68b8c2e17c1891f98ee4f7d8db80af779d
Tim Smith <tim.j.smith@genesys.com>
2018-06-04 11:57:31 -0600

    adding project

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const log = new (require('lognext'))('server');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+// Configure app
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+
+
+/* Routes */
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+// Start service
+app.listen(PORT, () => {
+	log.writeBox('The Unpapered Home\n\nCustom Order Form');
+	log.info(`Running on port ${PORT}`);
+});
